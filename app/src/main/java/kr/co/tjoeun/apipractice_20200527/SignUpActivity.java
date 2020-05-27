0bd2ae86568a52a442dca7b26084e6bddd24db7e
kj_cho <kj_cho@idealidea.co.kr>
2020-05-27 18:05:46 +0900

    비밀번호 체크 연습문제 제시

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
@@ -11,6 +11,18 @@ public class SignUpActivity extends BaseActivity {
 
     ActivitySignUpBinding binding;
 
+//    응용 문제.
+//    비번은 타이핑할때마다 길이 검사
+//    => 0글자 : 비밀번호를 입력해주세요.
+//    => 8글자 미만 : 비밀번호가 너무 짧습니다.
+//    그이상 : 사용해도 좋은 비밀번호 입니다.
+
+
+//    비번 확인도 타이핑할때마다 검사.
+//    => 0글자 : 비밀번호 확인을 입력해주세요.
+//    => 비번과 같다 : 비밀번호 재입력이 확인 되었습니다.
+//    => 다르다 : 비밀번호가 서로 다릅니다.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
