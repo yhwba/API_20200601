d4d58ad4d0be814ac3c7c518ff3f71ea31f140a2
kj_cho <kj_cho@idealidea.co.kr>
2020-05-27 15:11:53 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/BaseActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.apipractice_20200527;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+
+}
