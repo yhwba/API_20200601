d5f00f9971cf09e136c5d94bc0d0ee14f6ba8482
kj_cho <kj_cho@idealidea.co.kr>
2020-05-28 10:23:05 +0900

    비번 타이핑할때마다 => 회원가입 활성화 여부 검사

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
@@ -44,7 +44,7 @@ public class SignUpActivity extends BaseActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                checkPasswords();
+                checkSignUpEnable();
 
             }
 
@@ -64,7 +64,7 @@ public class SignUpActivity extends BaseActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                checkPasswords();
+                checkSignUpEnable();
 
             }
 
@@ -79,7 +79,10 @@ public class SignUpActivity extends BaseActivity {
 
 //    응용문제 => 비번 / 비번확인중 어느것을 타이핑해도 매번 둘다 검사.
 
-    void checkPasswords() {
+    boolean checkPasswords() {
+
+        boolean isPwOk = false;
+
         String pw = binding.pwEdt.getText().toString();
 
         if (pw.length() == 0) {
@@ -90,8 +93,10 @@ public class SignUpActivity extends BaseActivity {
         }
         else {
             binding.pwCheckResultTxt.setText("사용해도 좋은 비밀번호 입니다.");
+            isPwOk = true;
         }
 
+        boolean isPwRepeatOk = false;
         String pwRepeat = binding.pwRepeatEdt.getText().toString();
 
         if (pwRepeat.length() == 0) {
@@ -99,10 +104,21 @@ public class SignUpActivity extends BaseActivity {
         }
         else if (pwRepeat.equals(pw)) {
             binding.pwRepeatCheckResultTxt.setText("비밀번호 재입력이 확인 되었습니다.");
+            isPwRepeatOk = true;
         }
         else {
             binding.pwRepeatCheckResultTxt.setText("비밀번호가 서로 다릅니다.");
         }
+
+        return isPwOk && isPwRepeatOk;
+
+    }
+
+//    아이디중복 / 비번확인 / 닉네임 중복이 모두 통과여야, 회원가입버튼 활성화
+//    하나라도 틀리면 회원가입버튼 비활성화
+    void checkSignUpEnable() {
+
+        boolean isAllPasswordOk = checkPasswords();
     }
 
     @Override
