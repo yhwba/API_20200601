16ca3798d319ff94a0017f4f58067fa5a6c91e88
kj_cho <kj_cho@idealidea.co.kr>
2020-05-28 10:01:52 +0900

    비번확인 / 비번반복입력 확인 기능  - 다 맞춰놓고 비번만 바꾸면 => 달라짐을 인식 못함.

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/SignUpActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import kr.co.tjoeun.apipractice_20200527.databinding.ActivitySignUpBinding;
 
@@ -33,6 +35,64 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.pwEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String pw = s.toString();
+
+                if (pw.length() == 0) {
+                    binding.pwCheckResultTxt.setText("비밀번호를 입력해주세요.");
+                }
+                else if (pw.length() < 8) {
+                    binding.pwCheckResultTxt.setText("비밀번호가 너무 짧습니다.");
+                }
+                else {
+                    binding.pwCheckResultTxt.setText("사용해도 좋은 비밀번호 입니다.");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        binding.pwRepeatEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String pwRepeat = s.toString();
+
+                if (pwRepeat.length() == 0) {
+                    binding.pwRepeatCheckResultTxt.setText("비밀번호 확인을 입력해주세요.");
+                }
+                else if (pwRepeat.equals(binding.pwEdt.getText().toString())) {
+                    binding.pwRepeatCheckResultTxt.setText("비밀번호 재입력이 확인 되었습니다.");
+                }
+                else {
+                    binding.pwRepeatCheckResultTxt.setText("비밀번호가 서로 다릅니다.");
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
     }
 
