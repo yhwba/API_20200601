90d0a53ec05280d2685188e124f960b9572fafb6
kj_cho <kj_cho@idealidea.co.kr>
2020-05-29 14:49:21 +0900

    확인 버튼 눌러야 로그아웃 되도록

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/MainActivity.java
@@ -1,12 +1,15 @@
 package kr.co.tjoeun.apipractice_20200527;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -38,12 +41,30 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 
-                ContextUtil.setLoginUserToken(mContext, "");
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                alert.setTitle("로그아웃");
+                alert.setMessage("정말 로그아웃 하시겠습니까?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ContextUtil.setLoginUserToken(mContext, "");
 
-                Intent myIntent = new Intent(mContext, LoginActivity.class);
-                startActivity(myIntent);
+                        Intent myIntent = new Intent(mContext, LoginActivity.class);
+                        startActivity(myIntent);
 
-                finish();
+                        finish();
+                    }
+                });
+                alert.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "로그아웃을 취소했습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.show();
+
+
+
 
             }
         });
