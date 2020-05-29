a669d0896ab8c62b9da2441caab0031e2ff266dc
kj_cho <kj_cho@idealidea.co.kr>
2020-05-29 12:56:21 +0900

    스플래쉬액티비티 추가  - 시작점을 이 액티비티로 변경  - 액티비티의 테마를 NoActionBar로 변경

---
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/SplashActivity.java
@@ -0,0 +1,14 @@
+package kr.co.tjoeun.apipractice_20200527;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
