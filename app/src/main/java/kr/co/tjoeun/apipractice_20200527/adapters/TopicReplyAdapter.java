fd6fa4a5541e4d2fc8bc9da6fb3c462c78e21552
kj_cho <kj_cho@idealidea.co.kr>
2020-05-29 16:08:18 +0900

    주제에 달린 댓글 목록 뿌려주는 어댑터 => 뼈대 작업

---
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/adapters/TopicReplyAdapter.java
@@ -0,0 +1,43 @@
+package kr.co.tjoeun.apipractice_20200527.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import kr.co.tjoeun.apipractice_20200527.R;
+import kr.co.tjoeun.apipractice_20200527.datas.TopicReply;
+
+public class TopicReplyAdapter extends ArrayAdapter<TopicReply> {
+
+
+    Context mContext;
+    List<TopicReply> mList;
+    LayoutInflater inf;
+
+    public TopicReplyAdapter(@NonNull Context context, int resource, @NonNull List<TopicReply> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.topic_reply_list_item, null);
+        }
+
+
+        return row;
+    }
+}
