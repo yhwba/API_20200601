daccb09f5cf6156cf34da0d9c0cff26dc5eb24b0
kj_cho <kj_cho@idealidea.co.kr>
2020-06-01 09:49:31 +0900

    주제에 대한 의견 남기는 서버 요청 기능 추가

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/utils/ServerUtil.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/utils/ServerUtil.java
@@ -86,6 +86,61 @@ public class ServerUtil {
     }
 
 
+    public static void postRequestTopic(Context context, int topicId, String content, String side, final JsonResponseHandler handler) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new FormBody.Builder()
+                .add("topic_id", topicId+"")
+                .add("content", content)
+                .add("selected_side", side)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(BASE_URL + "/topic")
+                .post(requestBody)
+                .header("X-Http-Token", ContextUtil.getLoginUserToken(context))  // 헤더가 필요하다면 이 시점에서 첨부.
+                .build();
+
+
+//        client 를 이용해서 실제로 서버에 접근
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.e("서버연결실패", "로그인 기능 실패");
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                String body = response.body().string();
+
+                Log.d("서버연결성공", body);
+
+//                String body 를 JSONObject 로 변환.
+
+                try {
+                    JSONObject jsonObject = new JSONObject(body);
+
+//                    변환된 JSON을 액티비티에 전달 + 처리 실행.
+                    if (handler != null) {
+                        handler.onResponse(jsonObject);
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+    }
+
+
     public static void getRequestDuplicatedCheck(Context context, String input, String checkType, final JsonResponseHandler handler) {
 
         OkHttpClient client = new OkHttpClient();
