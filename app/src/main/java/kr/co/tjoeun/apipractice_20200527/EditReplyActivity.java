8cf68cba4dd92ff71eb3edf422ee9f705c24df14
kj_cho <kj_cho@idealidea.co.kr>
2020-06-01 10:30:39 +0900

    의견 남기기 기능 완료

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/EditReplyActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/EditReplyActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import kr.co.tjoeun.apipractice_20200527.databinding.ActivityEditReplyBinding;
 import kr.co.tjoeun.apipractice_20200527.datas.Topic;
+import kr.co.tjoeun.apipractice_20200527.utils.ServerUtil;
 
 public class EditReplyActivity extends BaseActivity {
 
@@ -24,7 +31,48 @@ public class EditReplyActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+//                토픽의 주제?
+
+                int topicId = mainTopic.getId();
+
+//                선택한 진영?
+
+                int selectedId = binding.sideRadioGroup.getCheckedRadioButtonId();
+
+                if (selectedId == -1) {
+                    Toast.makeText(mContext, "진영을 선택해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String side = ((RadioButton) findViewById(selectedId)).getText().toString();
+                Log.d("진영", side);
+
+//                입력한 내용?
+
+//                진영 미선택 / 내용이 10글자 이내면 등록 거부 처리. (응용문제)
+
+                if (binding.contentEdt.getText().length() < 10) {
+                    Toast.makeText(mContext, "의견은 10자 이상으로 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String content = binding.contentEdt.getText().toString();
+
+//                검사를 다 통과하면 서버에 요청.
+
+                ServerUtil.postRequestTopic(mContext, topicId, content, side, new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+                        Log.d("의견남기기응답", json.toString());
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
