c2e89ba1049e564eee75f7fba7183d98e88b79b2
yhwba <59465345+yhwba@users.noreply.github.com>
2020-06-01 11:41:34 +0900

    의견 남기기 성공시 메인화면으로 복귀

--- app/src/main/java/kr/co/tjoeun/apipractice_20200527/EditReplyActivity.java
+++ app/src/main/java/kr/co/tjoeun/apipractice_20200527/EditReplyActivity.java
@@ -1,14 +1,17 @@
 package kr.co.tjoeun.apipractice_20200527;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import kr.co.tjoeun.apipractice_20200527.databinding.ActivityEditReplyBinding;
@@ -37,7 +40,7 @@ public class EditReplyActivity extends BaseActivity {
 
 //                토픽의 주제?
 
-                int topicId = mainTopic.getId();
+                final int topicId = mainTopic.getId();
 
 //                선택한 진영?
 
@@ -48,7 +51,7 @@ public class EditReplyActivity extends BaseActivity {
                     return;
                 }
 
-                String side = ((RadioButton) findViewById(selectedId)).getText().toString();
+                final String side = ((RadioButton) findViewById(selectedId)).getText().toString();
                 Log.d("진영", side);
 
 //                입력한 내용?
@@ -60,16 +63,55 @@ public class EditReplyActivity extends BaseActivity {
                     return;
                 }
 
-                String content = binding.contentEdt.getText().toString();
+                final String content = binding.contentEdt.getText().toString();
 
 //                검사를 다 통과하면 서버에 요청.
 
-                ServerUtil.postRequestTopic(mContext, topicId, content, side, new ServerUtil.JsonResponseHandler() {
+                AlertDialog.Builder ab = new AlertDialog.Builder(mContext);
+                ab.setTitle("의견등록");
+                ab.setMessage("정말 의견을 남기시겠습니까? ");
+                ab.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onResponse(JSONObject json) {
-                        Log.d("의견남기기응답", json.toString());
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        ServerUtil.postRequestTopic(mContext, topicId, content, side, new ServerUtil.JsonResponseHandler() {
+                            @Override
+                            public void onResponse(JSONObject json) {
+                                Log.d("의견남기기응답", json.toString());
+
+                                try {
+                                    int code = json.getInt("code");
+                                    final String message = json.getString("message");
+                                    if(code ==200){
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                Toast.makeText(mContext, "message", Toast.LENGTH_SHORT).show();
+                                                finish();
+                                            }
+                                        });
+                                    }
+                                    else {
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                Toast.makeText(mContext, "message", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                    
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
                     }
                 });
+                ab.setNegativeButton("취소",null);
+                ab.show();
+
+
 
             }
         });
